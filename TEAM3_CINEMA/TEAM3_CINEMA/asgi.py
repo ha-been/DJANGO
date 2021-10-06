c758bb4f6d2c83a77a130812a01732430f0d728d
ha-been <im_habeenee@naver.com>
2021-10-06 22:05:00 +0900

    [PROJECT] 3TEAM CINEMA 영화예매사이트

---
+++ TEAM3_CINEMA/TEAM3_CINEMA/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TEAM3_CINEMA project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TEAM3_CINEMA.settings')
+
+application = get_asgi_application()
